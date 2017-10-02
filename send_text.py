f63fc702ad04920ec0c503595f89beec45734268
Analin Flores <foglzerika@gmail.com>
2017-10-02 18:32:04 -0500

    hidden sensitive data

--- send_text.py
+++ send_text.py
@@ -1,8 +1,8 @@
 from twilio.rest import Client
 
 #My Account Sid and auth token from twilio.com
-account = "AC7d3aac936d3ee4ab7c8253605d0d6eb8"
-token = "9c74be9665c070f09d6bda42dc18fc6e"
+account = "********"
+token = "********"
 
 client = Client(account,token)
 
